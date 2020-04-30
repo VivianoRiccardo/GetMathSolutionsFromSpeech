3e112e3a87d817166189c5c96b7ccd20eb7a13ac
Riccardo Viviano <40870708+VivianoRiccardo@users.noreply.github.com>
2020-04-30 21:19:56 +0200

    Add files via upload

---
+++ wolfram_alpha.py
@@ -0,0 +1,54 @@
+import wolframalpha
+from gtts import gTTS 
+import os
+import speech_recognition as sr
+
+f = open("substitutions.txt","r")
+l = f.readlines()
+d = {}
+for i in l:
+    k = i.strip().split(";")
+    d[k[0]] = k[1]
+f.close()
+# obtain audio from the microphone
+r = sr.Recognizer()
+while(True):
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source,1,10)
+    
+    text = ''
+    # recognize speech using Google Speech Recognition
+    try:
+        text = r.recognize_google(audio).lower()
+        l2 = text.split(" ")
+        l3 = []
+        for i in l2:
+            if i in d:
+                l3.append(d[i])
+            else:
+                l3.append(i)
+        text = ''
+        for i in l3:
+            text+=i+" "
+        print("Google Speech Recognition thinks you said " + text)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        text = "nothing"
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    
+    if text == "repeat" or text == "repeat ":
+        os.system("mplayer text.mp3")
+    elif text != "repeat" and text != "repeat " and text != "nothing":
+        #wolfram alpha computation
+        client = wolframalpha.Client("QWK49J-GR4UTVUPH7")
+        res = client.query(text)
+        print(next(res.results).text)
+        text = str(next(res.results).text)
+
+        # read it
+        language = "en"
+        speech = gTTS(text = text, lang = language, slow = False)
+        speech.save("text.mp3")
+        os.system("mplayer text.mp3")
